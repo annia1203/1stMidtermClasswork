f7f434eaed84497a04f6b56edc9774c75bac1b50
annia1203 <81383776+annia1203@users.noreply.github.com>
2022-10-06 13:56:30 -0500

    Add files via upload

---
+++ Nodetest/packagesex/index.js
@@ -0,0 +1,12 @@
+var sh = require("superheroes");
+
+var mySHname = sh.random();
+
+console.log("Here comes " + mySHname);
+
+var sv = require("supervillains");
+
+var mySVname = sv.random();
+
+console.log("The supervillian fighting today is " + mySVname + " against " + mySHname + ". " + mySHname + ", please rescue us!");
+
